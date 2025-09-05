f54e0072858e9f347dca989ae95ed213234e4fda
KDDevGIT <kymasterbox@gmail.com>
2025-09-05 14:43:56 -0700

    Added variables.tf module into modules/iam_baseline

---
+++ modules/iam_baseline/variables.tf
@@ -0,0 +1,35 @@
+# MODULE
+variable "account_alias" {
+  type = string
+}
+
+variable "password_policy" {
+  type = any
+}
+
+variable "users" {
+  type = map(any)
+}
+
+variable "groups" {
+  type = map(any)
+}
+
+variable "managed_policies" {
+  type = map(any)
+}
+
+variable "group_membership" {
+  type = map(list(string))
+}
+
+variable "enforce_mfa_policy" {
+  type = bool
+}
+
+variable "tags" {
+  type = map(string)
+}
+
+
+
